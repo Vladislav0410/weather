0a4dda5a0e4ea36fce6a5985e068c591ce0b030a
Vladislav0410 <Vladkovalev0410@gmail.com>
2021-01-31 00:16:44 +0300

    optimization

--- src/App.js
+++ src/App.js
@@ -1,80 +1,93 @@
-import React from 'react';
-import Info from './components/info';
-import Form from './components/form';
-import Weather from './components/weather';
+import React from "react";
+import Info from "./components/info";
+import Form from "./components/form";
+import Weather from "./components/weather";
 
 class App extends React.Component {
+	state = {
+		temp: "",
+		city: "",
+		country: "",
+		windspeed: "",
+		description: "",
+		humidity: "",
+		error: "",
+	};
 
-  state = {
-    temp: "",
-    city: "",
-    country: "",
-    windspeed: "",
-    description: "",
-    humidity: "",
-    error: ""
-  }
-
-  gettingWeather = async (e) => {
-    e.preventDefault();
-    let city = e.target.elements.city.value;
-    if(city) {
-      const api = await fetch(`HTTPS://api.openweathermap.org/data/2.5/weather?q=${city}&appid=0ceeb6d2cd47c2a59d70272403971e35&units=metric`);
-      const data = await api.json();  
-      console.log(data)
-      try{
-      this.setState({
-        temp: Math.round(data.main.temp),
-        city: data.name,
-        country: data.sys.country,
-        windspeed: Math.round(data.wind.speed),
-        description: data.weather[0].description[0].toUpperCase() + data.weather[0].description.slice(1),
-        humidity: data.main.humidity,
-        error: ""
-      });
-    } catch (e) {
-      alert('Enter correct city name')
-      window.location.reload();
-    }
-    } else {
-      this.setState({
-    temp: "",
-    city: "",
-    country: "",
-    windspeed: "",
-    description: "",
-    humidity: "",
-    error: "Enter city name"
-      });
-    }
-  }
-  render() {
-    return (
-      <div className="wrapper">
-        <div className="main">
-          <div className="container">
-            <div className="row">
-              <div className="col-sm-5 info">
-              <Info />
-              </div>
-              <div className="col-sm-7 form">
-              <Form weatherMethod={this.gettingWeather} />
-              <Weather
-                temp={this.state.temp}
-                city={this.state.city}
-                country={this.state.country}
-                windspeed={this.state.windspeed}
-                description={this.state.description}
-                humidity={this.state.humidity}
-                error={this.state.error}
-              />
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    );
-  }
+	gettingWeather = async (e) => {
+		e.preventDefault();
+		const city = e.target.elements.city.value;
+		if (city) {
+			try {
+                const api = await fetch(
+                    `HTTPS://api.openweathermap.org/data/2.5/weather?q=${city}&appid=0ceeb6d2cd47c2a59d70272403971e35&units=metric`
+                );
+                const data = await api.json();
+                if(data.cod === "404") {
+                    throw "Enter correct city name";
+                }
+                    const {main, sys, weather, wind, name} = data;
+                    this.setState({
+                        temp: Math.round(main.temp),
+                        city: name,
+                        country: sys.country,
+                        windspeed: Math.round(wind.speed),
+                        description:
+                            weather[0].description[0].toUpperCase() +
+                            weather[0].description.slice(1),
+                        humidity: main.humidity,
+                        error: "",
+                    });
+			} catch (e) {
+                this.setState({
+                    temp: "",
+                    city: "",
+                    country: "",
+                    windspeed: "",
+                    description: "",
+                    humidity: "",
+                    error: e,
+                });
+			}
+		} else {
+			this.setState({
+				temp: "",
+				city: "",
+				country: "",
+				windspeed: "",
+				description: "",
+				humidity: "",
+				error: "Enter city name",
+			});
+		}
+	};
+	render() {
+		return (
+			<div className="wrapper">
+				<div className="main">
+					<div className="container">
+						<div className="row">
+							<div className="col-sm-5 info">
+								<Info />
+							</div>
+							<div className="col-sm-7 form">
+								<Form weatherMethod={this.gettingWeather} />
+								<Weather
+									temp={this.state.temp}
+									city={this.state.city}
+									country={this.state.country}
+									windspeed={this.state.windspeed}
+									description={this.state.description}
+									humidity={this.state.humidity}
+									error={this.state.error}
+								/>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
